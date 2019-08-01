86fa3c82b96d138c885c87dabd9bfd139200c29f
pfenders <pfenders3123@gmail.com>
2019-07-31 22:46:27 -0700

    update

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
-function check(){
-const question1 = document.quiz.question1.value;
-const question2 = document.quiz.question2.value;
-const question3 = document.quiz.question3.value;
-let correct = 0;
+function check() {
+    const question1 = document.quiz.question1.value;
+    const question2 = document.quiz.question2.value;
+    const question3 = document.quiz.question3.value;
+    let correct = 0;
 
-if (question1 == "Blue"){
-    correct++;
-}
-if (question2 == "Red"){
-    correct++;
-}
-if (question3 == "Honey"){
-    correct++;
-}
+    if (question1 == "Blue") {
+        correct++;
+    }
+    if (question2 == "Red") {
+        correct++;
+    }
+    if (question3 == "Honey") {
+        correct++;
+    }
 
-//Custom Picture Based on how well you do
-const messages =["Great Job", "Not Bad", "Good Luck Next Time"];
-const pictures =["images/Good-Job.png", "images/Not-Bad.png", "images/Sorry.png"];
+    //Custom Picture Based on how well you do
+    const messages = ["Great Job", "Not Bad", "Good Luck Next Time"];
+    const pictures = ["images/Good-Job.png", "images/Not-Bad.png", "images/Sorry.png"];
 
-let range;
+    let range;
 
-if (correct < 1) {
-    range = 2;
-}
-if (correct > 0 && correct < 3) {
-    range = 1;
-}
-if (correct > 2) {
-    range = 0;
-}
+    if (correct < 1) {
+        range = 2;
+    }
+    if (correct > 0 && correct < 3) {
+        range = 1;
+    }
+    if (correct > 2) {
+        range = 0;
+    }
 
-document.getElementById("quiz-submit").style.visibility ="visible";
-
-document.getElementById("message").innerHTML = messages[range];
-document.getElementById("number_of_correct_answer").innerHTML = "You got " + correct + " correct.";
-document.getElementById("picture").src = pictures[range];
+    document.getElementById("quiz-submit").style.visibility = "visible";
+    document.getElementById("message").innerHTML = messages[range];
+    document.getElementById("number_of_correct_answer").innerHTML = "You got " + correct + " correct.";
+    document.getElementById("picture").src = pictures[range];
 }
