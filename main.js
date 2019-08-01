8d76a810d2833f9c1c043f4be7b392bdce84e6b4
pfenders <pfenders3123@gmail.com>
2019-07-31 21:28:32 -0700

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function check(){
 const question1 = document.quiz.question1.value;
 const question2 = document.quiz.question2.value;
 const question3 = document.quiz.question3.value;
-let correct = 0 ;
+let correct = 0;
 
 if (question1 == "Blue"){
     correct++;
@@ -14,6 +14,18 @@ if (question3 == "Honey"){
     correct++;
 }
 
-document.getElementById("quiz-submit").style.visibility="visible";
+//Custom Picture Based on how well you do
+
+if (correct < 1) {
+    range = 2;
+}
+if (correct > 0 && correct < 3) {
+    score = 1;
+}
+if (correct > 2) {
+    score = 0;
+}
+
+document.getElementById("quiz-submit").style.visibility ="visible";
 document.getElementById("number_of_correct_answer").innerHTML = "You got " + correct + " correct.";
 }
